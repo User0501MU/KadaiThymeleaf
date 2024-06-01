dda10400cce9c97cd22434a252949cc4597546ec
User0501MU <m-utsunomiya@fores.jp>
2024-06-02 07:36:13 +0900

    画面に固定で表示するテキストは、プロパティファイルから取得して画面表示するように実装してください。

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,6 +3,7 @@ package com.techacademy;
 
 //メソッド名: getInput 処理内容: 入力画面の初期表示をする
 
+/*
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,4 +23,24 @@ public class InputController {
 		// inputpit.htmlに画面遷移
 		return "input";
 	}
+}
+*/
+
+
+//InputController.java
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InputController {
+
+ @GetMapping("/input")
+ public String getInput(Model model) {
+     // プロパティファイルからテキストを取得してモデルに追加
+     model.addAttribute("welcomeMessage", "Thymeleaf課題");
+     //画面に固定で表示するテキストは、プロパティファイルから取得して画面表示するように実装してください。
+     return "input";
+ }
 }
