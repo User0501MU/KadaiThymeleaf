15e28ee07f481c631d72f1ee000e4f4ded8da8b6
User0501MU <m-utsunomiya@fores.jp>
2024-06-02 07:12:34 +0900

    KadaiThymeleaf

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,25 @@
+package com.techacademy;
+//InputController
+
+//メソッド名: getInput 処理内容: 入力画面の初期表示をする
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+//インポート文解決＞springbootで作成しなさい！control＋shift＋O
+
+//POSTメソッドでoutputに送信する処理
+
+@Controller
+public class InputController {
+
+	@GetMapping("/input")
+	// @GetMapping("/") アノテーションは、URL
+	// http://localhost:8080/（「/」のアドレス）に対する「GETメソッド」を受け取る関数であることを示します。
+	public String getInput() {
+
+		// inputpit.htmlに画面遷移
+		return "input";
+	}
+}
---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,22 @@
+package com.techacademy;
+//OutputController
+//メソッド名: postOutput 処理内容: 入力画面から入力値を受け取って、出力画面を表示する
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+	@Controller
+	public class OutputController {
+
+		@PostMapping("/Output")
+		//@GetMapping("/") アノテーションは、URL http://localhost:8080/（「/」のアドレス）に対する「GETメソッド」を受け取る関数であることを示します。
+		public String getInput(@RequestParam("val")String val,Model model) {
+			model.addAttribute("previous",val);//val任意
+	        // inputpit.htmlに画面遷移
+	        return "Output";
+	    }
+
+	}
