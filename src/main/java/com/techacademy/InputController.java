eeeb47537865239ac40ef2b64ae3d4182bcc7316
User0501MU <m-utsunomiya@fores.jp>
2024-06-03 11:41:12 +0900

    「画面に固定で表示するテキストは、プロパティファイルから取得して画面表示するように実装してください。」＞msgファイル作成プリパティをinput.htmlに記述変更。

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,11 +3,10 @@ package com.techacademy;
 
 //メソッド名: getInput 処理内容: 入力画面の初期表示をする
 
-/*
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.ui.Model;
 //インポート文解決＞springbootで作成しなさい！control＋shift＋O
 
 //POSTメソッドでoutputに送信する処理
@@ -23,24 +22,4 @@ public class InputController {
 		// inputpit.htmlに画面遷移
 		return "input";
 	}
-}
-*/
-
-
-//InputController.java
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class InputController {
-
- @GetMapping("/input")
- public String getInput(Model model) {
-     // プロパティファイルからテキストを取得してモデルに追加
-     model.addAttribute("welcomeMessage", "Thymeleaf課題");
-     //画面に固定で表示するテキストは、プロパティファイルから取得して画面表示するように実装してください。
-     return "input";
- }
 }
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -1,6 +1,4 @@
 package com.techacademy;
-
-/*
 //OutputController
 //メソッド名: postOutput 処理内容: 入力画面から入力値を受け取って、出力画面を表示する
 
@@ -22,22 +20,3 @@ import org.springframework.web.bind.annotation.RequestParam;
 	    }
 
 	}
-*/
-
-
-//OutputController.java
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class OutputController {
-
- @PostMapping("/output")
- public String postOutput(@RequestParam("inputValue") String inputValue, Model model) {
-     // 入力値を受け取ってモデルに追加
-     model.addAttribute("inputValue", inputValue);
-     return "output";
- }
-}
